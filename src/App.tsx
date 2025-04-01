c5ce8ec3bead3b9e52f5456e8136cc09c0e9463f
Adam Jednacz <adamjednacz4@gmail.com>
2025-04-01 20:59:58 +0200

    modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,12 @@
-import { useState } from "react";
-import { postForResult } from "./api/post";
-import { Button } from "./components/ui/button";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [result, setResult] = useState<number | null>(null);
-  const [number1, setNumber1] = useState<number | null>(null);
-  const [number2, setNumber2] = useState<number | null>(null);
-
-  const fetchResult = async () => {
-    if (!number1 || !number2) return;
-
-    try {
-      const newResult = await postForResult({ number1, number2 });
-
-      console.log(newResult);
-
-      setResult(newResult.result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
-      <Button variant="default">Siema</Button>
-      <h3 className="bg-red-500">Siemano, masz tu kalkulator</h3>
-      <input
-        type="number"
-        onChange={(e) => setNumber1(parseFloat(e.currentTarget.value))}
-        value={number1 || ""}
-      />
-      {" + "}
-      <input
-        type="number"
-        onChange={(e) => setNumber2(parseFloat(e.currentTarget.value))}
-        value={number2 || ""}
-      />
-      {" = "}
-      <span>{result}</span>
-      <button onClick={fetchResult}>Policz</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
