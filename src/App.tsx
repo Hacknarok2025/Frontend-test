d75ab7e5bae2318fc30a9ca4b3eb0f321d503972
Przemysław Zieliński <zprzemek378@gmail.com>
2025-04-01 20:11:09 +0200

    remove previous App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
-import { postForResult } from "./api/post";
-import { Button } from "./components/ui/button";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
-  const [result, setResult] = useState<number | null>(null);
-  const [number1, setNumber1] = useState<number | null>(null);
-  const [number2, setNumber2] = useState<number | null>(null);
-
-  const fetchResult = async () => {
-    if (!number1 || !number2) return;
-
-    try {
-      const newResult = await postForResult({ number1, number2 });
-
-      console.log(newResult);
-
-      setResult(newResult.result);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   return (
     <>
       <Routes>
