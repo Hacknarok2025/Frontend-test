9ba8453052f986110144f05775769de0562600af
Przemysław Zieliński <zprzemek378@gmail.com>
2025-04-12 17:05:02 +0200

    repair sizes

--- src/components/own/button.tsx
+++ src/components/own/button.tsx
@@ -1,22 +1,16 @@
-
-
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    size?: string;
-    add?:any
+  add?: any;
 }
 
-const Button = ({ children, onClick, size,add }: ButtonProps) => {
-    return (
-        <button
-            onClick={onClick}
-            className={`${add} m-5 bg-[#000000] text-${size}  px-6 py-3  transition-all duration-500 transform hover:scale-110 skew-x-12`}
-        >
-            <span className="inline-block skew-x-[-12deg]">
-                {children}
-            </span>
-        </button>
-    );
+const Button = ({ children, onClick, add }: ButtonProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`m-5 bg-[#000000] px-6 py-3 transition-all duration-500 transform hover:scale-110 skew-x-12 ${add}`}
+    >
+      <span className="inline-block skew-x-[-12deg]">{children}</span>
+    </button>
+  );
 };
 
 export default Button;
-;
