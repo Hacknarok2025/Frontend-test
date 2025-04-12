48fe1c1c6aeb51eba908940033017e5b72667626
Laptop <adamjednacz4@gmail.com>
2025-04-12 17:32:01 +0200

    naprawa

--- src/commons/Modal1.tsx
+++ src/commons/Modal1.tsx
@@ -1,20 +1,14 @@
-<<<<<<< HEAD:src/commons/Modal1.tsx
+
 import { motion, AnimatePresence } from "framer-motion";
 import Button from "@/components/own/button";
 import Input from "@/components/own/input";
 import {useNavigate} from "react-router-dom";
-=======
-import { motion, AnimatePresence } from 'framer-motion';
-import Button from '@/components/own/button';
-import Input from '@/components/own/input';
->>>>>>> main:src/commons/Modal.tsx
 
 interface ModalProps {
   open: boolean;
   onClose: () => void;
 }
 
-<<<<<<< HEAD:src/commons/Modal1.tsx
 const Modal1: React.FC<ModalProps> = ({ open, onClose }) => {
     const navigate = useNavigate(); // Używamy useNavigate zamiast useRouter
 
@@ -80,62 +74,3 @@ const Modal1: React.FC<ModalProps> = ({ open, onClose }) => {
 };
 
 export default Modal1;
-=======
-const Modal: React.FC<ModalProps> = ({ open, onClose }) => {
-  return (
-    <AnimatePresence>
-      {open && (
-        <motion.div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black/70"
-          onClick={onClose}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-          transition={{ duration: 0.3 }}
-        >
-          <motion.div
-            className="bg-white p-8 w-1/2 max-w-2xl"
-            onClick={(e) => e.stopPropagation()}
-            initial={{ scale: 0.5, opacity: 0, skewX: 12 }}
-            animate={{ scale: 1, opacity: 1, skewX: 12 }}
-            exit={{ scale: 0.5, opacity: 0, skewX: 12 }}
-            style={{ transformOrigin: 'center' }}
-            transition={{
-              type: 'spring',
-              damping: 20,
-              stiffness: 100,
-              duration: 0.3,
-            }}
-          >
-            <div className="flex justify-between items-start mb-6 skew-x-[-24deg]">
-              <h1 className="text-2xl font-bold">Select Your Warriors Name</h1>
-              <button
-                onClick={onClose}
-                className="text-gray-500 hover:text-gray-700 text-4xl"
-              >
-                ×
-              </button>
-            </div>
-            <form className="flex justify-center flex-col items-start">
-              <Input placeholder={'Name'} size={'2xl'} />
-              <Input placeholder={'Email'} size={'2xl'} />
-            </form>
-            <div className="mt-6 flex justify-end skew-x-[-24deg]">
-              <Button
-                onClick={onClose}
-                add={
-                  'px-18 hover:bg-white hover:text-black skew-x-[24deg] border-2 border-solid border-black text-white text-2xl'
-                }
-              >
-                Start
-              </Button>
-            </div>
-          </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
-  );
-};
-
-export default Modal;
->>>>>>> main:src/commons/Modal.tsx
