5b025c9efaf08d4621a05621d72589542a1670a4
Laptop <adamjednacz4@gmail.com>
2025-04-13 09:36:33 +0200

    lvl9

--- src/pages/level9/utils/controls.ts
+++ src/pages/level9/utils/controls.ts
@@ -5,24 +5,24 @@ import { MOVEMENT_SPEED, JUMP_FORCE, ATTACK_WIDTH, ATTACK_HEIGHT, PLAYER_ATTACK_
 // Handle player keyboard controls
 export const handlePlayerControls = (p5: p5Types, player: Entity, onAttack: (attack: Attack) => void) => {
   // Movement
-  if (p5.keyIsDown(p5.LEFT_ARROW)) {
+  if (p5.keyIsDown(p5.LEFT_ARROW) || p5.keyIsDown(65)) { // 65 = A key
     player.x -= MOVEMENT_SPEED;
     player.direction = -1;
   }
-  
-  if (p5.keyIsDown(p5.RIGHT_ARROW)) {
+
+  if (p5.keyIsDown(p5.RIGHT_ARROW) || p5.keyIsDown(68)) { // 68 = D key
     player.x += MOVEMENT_SPEED;
     player.direction = 1;
   }
-  
-  // Jump (spacebar)
-  if (p5.keyIsDown(32) && !player.jumping) { // 32 is spacebar
+
+  // Jump (up arrow or W key)
+  if ((p5.keyIsDown(p5.UP_ARROW) || p5.keyIsDown(87)) && !player.jumping) { // 87 = W key
     player.velocity = JUMP_FORCE;
     player.jumping = true;
   }
-  
+
   // Attack (X key)
-  if (p5.keyIsDown(88) && player.attackCooldown <= 0) { // 88 is 'X'
+  if (p5.keyIsDown(32) && player.attackCooldown <= 0) {
     player.attacking = true;
     player.attackCooldown = PLAYER_ATTACK_COOLDOWN;
     
